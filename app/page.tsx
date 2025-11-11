d6b8ee462ed9a62af1f30dad99fa01879839c91f
spoonenter <spoon_enter@naver.com>
2025-11-11 15:31:46 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import type React from "react"
-
+import AdComponent from "@/components/AdComponent";
 import { useState, useEffect } from "react"
 import { Button } from "@/components/ui/button"
 import { Card } from "@/components/ui/card"
